8b90b9808a3e6c7a670868642cdbc224474ead41
ardier16 <ardier16@gmail.com>
2023-12-22 17:55:22 +0200

    Update auth layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,17 +1,26 @@
-import { Box, Stack } from '@mui/material'
+import { Box, BoxProps, Stack } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 
 import { config } from '@/config'
 
 const AuthLayout = () => {
+  const bgBoxProps: BoxProps = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: -1,
+  }
+
   return (
     <Stack
       alignItems='center'
       justifyContent='center'
       height='100vh'
       width='100%'
-      bgcolor='#fff'
       sx={{
+        backgroundColor: 'background.paper',
         backgroundImage: 'url("/imgs/app-bg.png")',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'right',
@@ -20,42 +29,24 @@ const AuthLayout = () => {
       }}
     >
       <Box
+        {...bgBoxProps}
         sx={{
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          background: 'var(--col-bg)',
+          backgroundColor: 'background.default',
           opacity: 0.25,
           filter: 'blur(2px)',
-          zIndex: 'var(--z-index-hidden)',
         }}
       />
 
       <Box
-        sx={{
-          background: 'url("/imgs/cells-bg.png") repeat top center / contain',
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100%',
-          height: '100%',
-          zIndex: 'var(--z-index-hidden)',
-        }}
+        {...bgBoxProps}
+        sx={{ background: 'url("/imgs/cells-bg.png") repeat top center / contain' }}
       />
 
-      <Box
-        sx={{
-          position: 'absolute',
-          top: '25px',
-          left: '65px',
-        }}
-      >
+      <Box position='absolute' top={25} left={25}>
         <img src='/branding/logo-sign-in.svg' alt={config.APP_NAME} />
       </Box>
 
-      <Stack flexDirection='column' alignItems='center' justifyContent='center'>
+      <Stack direction='column' alignItems='center' justifyContent='center'>
         <Outlet />
       </Stack>
     </Stack>
