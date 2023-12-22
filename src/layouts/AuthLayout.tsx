d81e79124b49b719ae42566e8b907437af073e3e
ardier16 <ardier16@gmail.com>
2023-12-22 18:22:55 +0200

    Update auth layout styles

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,18 +1,9 @@
-import { Box, BoxProps, Stack } from '@mui/material'
+import { Box, Stack } from '@mui/material'
 import { Outlet } from 'react-router-dom'
 
 import { config } from '@/config'
 
 const AuthLayout = () => {
-  const bgBoxProps: BoxProps = {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    zIndex: -1,
-  }
-
   return (
     <Stack
       alignItems='center'
@@ -20,28 +11,11 @@ const AuthLayout = () => {
       height='100vh'
       width='100%'
       sx={{
-        backgroundColor: 'background.paper',
         backgroundImage: 'url("/imgs/app-bg.png")',
         backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'right',
-        backgroundSize: '50% / cover',
-        backdropFilter: 'opacity(0.1)',
+        backgroundSize: 'cover',
       }}
     >
-      <Box
-        {...bgBoxProps}
-        sx={{
-          backgroundColor: 'background.default',
-          opacity: 0.25,
-          filter: 'blur(2px)',
-        }}
-      />
-
-      <Box
-        {...bgBoxProps}
-        sx={{ background: 'url("/imgs/cells-bg.png") repeat top center / contain' }}
-      />
-
       <Box position='absolute' top={25} left={25}>
         <img src='/branding/logo-sign-in.svg' alt={config.APP_NAME} />
       </Box>
