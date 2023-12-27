cf407e9fbf008c94406367fb35303450027a78d3
nikitabelous <nikitabelous16@gmail.com>
2023-12-27 14:03:45 +0200

    feat add setting userDid to mm-zkp context

--- src/contexts/metamask-zkp-snap.tsx
+++ src/contexts/metamask-zkp-snap.tsx
@@ -18,6 +18,8 @@ import { createContext, FC, HTMLAttributes, useCallback, useState } from 'react'
 interface MetamaskZkpSnapContextValue {
   isMetamaskInstalled: boolean
   isSnapInstalled: boolean
+  userDid: string
+  userDidBigIntString: string
 
   isLocalSnap: (snapId: string) => boolean
 
@@ -82,6 +84,8 @@ export const MetamaskZkpSnapContextProvider: FC<HTMLAttributes<HTMLDivElement>> 
 
   const [isMetamaskInstalled, setIsMetamaskInstalled] = useState(false)
   const [isSnapInstalled, setIsSnapInstalled] = useState(false)
+  const [userDid, setUserDid] = useState('')
+  const [userDidBigIntString, setUserDidBigIntString] = useState('')
 
   const isLocalSnap = useCallback((snapId: string) => snapId.startsWith('local:'), [])
 
@@ -91,8 +95,10 @@ export const MetamaskZkpSnapContextProvider: FC<HTMLAttributes<HTMLDivElement>> 
    */
   const createIdentity = useCallback(async () => {
     if (!connector) throw new TypeError('Connector is not defined')
-
-    return connector.createIdentity()
+    const identity = await connector.createIdentity()
+    setUserDid(identity.identityIdString)
+    setUserDidBigIntString(identity.identityIdBigIntString)
+    return identity
   }, [connector])
 
   /**
@@ -163,6 +169,8 @@ export const MetamaskZkpSnapContextProvider: FC<HTMLAttributes<HTMLDivElement>> 
       value={{
         isMetamaskInstalled,
         isSnapInstalled,
+        userDid,
+        userDidBigIntString,
 
         isLocalSnap,
 
