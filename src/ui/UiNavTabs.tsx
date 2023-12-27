4f16b6af34e7c116d3f956adfa1197497279674e
lukachi <lukachisama@gmail.com>
2023-12-27 12:16:44 +0200

    refactored navtabs

--- src/ui/UiNavTabs.tsx
+++ src/ui/UiNavTabs.tsx
@@ -1,6 +1,6 @@
 import { ButtonProps, Stack, StackProps } from '@mui/material'
-import { useCallback } from 'react'
-import { NavLink, useLocation } from 'react-router-dom'
+import { useMemo } from 'react'
+import { NavLink, NavLinkProps, useLocation } from 'react-router-dom'
 
 import UiButton from '@/ui/UiButton'
 
@@ -11,22 +11,35 @@ interface Props extends StackProps {
   }[]
 }
 
-export default function UiNavTabs({ tabs, ...rest }: Props) {
+function NavTab({ to, label, ...rest }: { label: string } & NavLinkProps) {
   const location = useLocation()
 
-  // FIXME
-  const getLinkProps = useCallback(
-    (route: string): Partial<ButtonProps> => {
-      const isRouteActive = location.pathname.includes(route)
+  const getLinkProps = useMemo((): Partial<ButtonProps> => {
+    const isRouteActive = location.pathname.includes(to as string)
 
-      return {
-        variant: isRouteActive ? 'contained' : 'text',
-        color: isRouteActive ? 'primary' : 'secondary',
-      }
-    },
-    [location?.pathname],
+    return {
+      variant: isRouteActive ? 'contained' : 'text',
+      color: isRouteActive ? 'primary' : 'secondary',
+    }
+  }, [location.pathname, to])
+
+  return (
+    <NavLink {...rest} to={to}>
+      <UiButton
+        sx={theme => ({
+          background: theme.palette.background.default,
+          px: 7,
+          py: 2,
+        })}
+        {...getLinkProps}
+      >
+        {label}
+      </UiButton>
+    </NavLink>
   )
+}
 
+export default function UiNavTabs({ tabs, ...rest }: Props) {
   return (
     <Stack
       {...rest}
@@ -40,19 +53,8 @@ export default function UiNavTabs({ tabs, ...rest }: Props) {
       })}
       padding={1}
     >
-      {tabs.map(({ route, label }) => (
-        <NavLink key={route} to={route}>
-          <UiButton
-            sx={theme => ({
-              background: theme.palette.background.default,
-              px: 7,
-              py: 2,
-            })}
-            {...getLinkProps(route)}
-          >
-            {label}
-          </UiButton>
-        </NavLink>
+      {tabs.map(({ route, label }, idx) => (
+        <NavTab key={idx} to={route} label={label} />
       ))}
     </Stack>
   )
