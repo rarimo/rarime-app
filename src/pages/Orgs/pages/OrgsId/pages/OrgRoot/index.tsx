d3a8d49975059d072795aa6c99012acf2f5f6914
ardier16 <ardier16@gmail.com>
2023-12-28 18:11:13 +0200

    Fix check proof link

--- src/pages/Orgs/pages/OrgsId/pages/OrgRoot/index.tsx
+++ src/pages/Orgs/pages/OrgsId/pages/OrgRoot/index.tsx
@@ -1,18 +1,19 @@
 import { Box, Stack } from '@mui/material'
-import { NavLink, useParams } from 'react-router-dom'
+import { generatePath, NavLink, useParams } from 'react-router-dom'
 
 import { PageTitles } from '@/common'
+import { RoutePaths } from '@/enums'
 import { UiButton } from '@/ui'
 
 export default function OrgRoot() {
-  const { id } = useParams<{ id: string }>()
+  const { id = null } = useParams<{ id: string }>()
 
   return (
     <Stack flex={1}>
       <PageTitles title={`Organization ${id}`} subtitle='Some organization description' />
 
       <Box mt={6}>
-        <NavLink to={`/organisations/${id}/check-proof`}>
+        <NavLink to={generatePath(RoutePaths.OrgsIdCheckProof, { id })}>
           <UiButton component='div'>Check proof</UiButton>
         </NavLink>
       </Box>
