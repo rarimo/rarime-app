98bfbbecfeb050c9d7daa914617e34aa0b9285b6
Maksym <ardier16@gmail.com>
2024-01-22 12:33:44 +0200

    Update API endpoints (#23)
    * Update API endpoints
    
    * Extract service paths to enum
    
    * Use pascal case for enum

--- src/api/modules/orgs/helpers/org-groups-requests.ts
+++ src/api/modules/orgs/helpers/org-groups-requests.ts
@@ -17,6 +17,7 @@ import {
   OrgUserRoles,
   VCSchema,
 } from '@/api'
+import { ApiServicePaths } from '@/enums/api'
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const DUMMY_ORG_GROUP_REQUESTS: OrgGroupRequest[] = [
@@ -436,7 +437,7 @@ const fakeLoadRequestsAll = async (query?: OrgGroupRequestQueryParams) => {
 
 export const createInvitation = async ({ orgId, groupId, email, rules }: OrgGroupCreateRequest) => {
   const { data } = await api.post<OrgGroupCreatedRequest>(
-    `/v1/orgs/${orgId}/groups/${groupId}/emails`,
+    `${ApiServicePaths.Orgs}/v1/orgs/${orgId}/groups/${groupId}/emails`,
     {
       body: {
         data: {
@@ -467,7 +468,7 @@ export const acceptInvitation = async ({
   userDid: string
 }) => {
   const { data } = await api.patch<OrgGroupCreatedRequest>(
-    `/v1/orgs/${orgId}/groups/${groupId}/emails`,
+    `${ApiServicePaths.Orgs}/v1/orgs/${orgId}/groups/${groupId}/emails`,
     {
       body: {
         data: {
@@ -487,7 +488,8 @@ export const acceptInvitation = async ({
 }
 
 export const loadOrgGroupRequests = async (query?: OrgGroupRequestQueryParams) => {
-  // const { data } = await api.get<OrgGroupRequest[]>(`/v1/orgs/requests`, {
+  // const { data } =
+  // await api.get<OrgGroupRequest[]>(`${ApiServicePaths.Orgs}/v1/orgs/requests`, {
   //   query: query,
   // })
   //
@@ -498,7 +500,7 @@ export const loadOrgGroupRequests = async (query?: OrgGroupRequestQueryParams) =
 
 export const loadOrgGroupRequestById = async (orgId: string, groupId: string, reqId: string) => {
   const { data } = await api.get<OrgGroupRequest>(
-    `/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
+    `${ApiServicePaths.Orgs}/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
   )
 
   return data
@@ -517,7 +519,7 @@ export const fillOrgGroupRequest = async ({
   credReq: CredentialRequest[]
 }) => {
   const { data } = await api.patch<OrgGroupRequest>(
-    `/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
+    `${ApiServicePaths.Orgs}/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
     {
       body: {
         data: {
@@ -551,7 +553,7 @@ export const verifyOrgGroupRequest = async ({
   metadata: OrgGroupVCsMetadata
 }) => {
   const { data } = await api.post<OrgGroupRequest>(
-    `/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
+    `${ApiServicePaths.Orgs}/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
     {
       body: {
         data: {
@@ -581,7 +583,7 @@ export const rejectOrgGroupRequest = async ({
   reqId: string
 }) => {
   const { data } = await api.post<OrgGroupRequest>(
-    `/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
+    `${ApiServicePaths.Orgs}/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}`,
     {
       body: {
         data: {
@@ -607,7 +609,7 @@ export const getOrgGroupPublishingRequests = async ({
   reqId: string
 }): Promise<OrgGroupRequestPublishing[]> => {
   const { data } = await api.get<OrgGroupRequestPublishing[]>(
-    `/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}/publishing`,
+    `${ApiServicePaths.Orgs}/v1/orgs/${orgId}/groups/${groupId}/requests/${reqId}/publishing`,
   )
 
   return data
@@ -637,7 +639,9 @@ export const loadAndParseCredentialSchema = async (
 export const loadOrgGroupReqMetadataById = async (
   metadataId: string,
 ): Promise<OrgGroupVCsMetadata> => {
-  const { data } = await api.get<OrgGroupVCsMetadata>(`/v1/orgs/metadata/${metadataId}`)
+  const { data } = await api.get<OrgGroupVCsMetadata>(
+    `${ApiServicePaths.Orgs}/v1/orgs/metadata/${metadataId}`,
+  )
 
   return data
 
