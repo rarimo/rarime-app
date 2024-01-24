48e3153a02cded5be5feb56e6c105de80cfdfbc8
ardier16 <ardier16@gmail.com>
2024-01-24 14:01:55 +0200

    Add no data viewer

---
+++ src/common/NoDataViewer.tsx
@@ -0,0 +1,54 @@
+import { Stack, StackProps, Typography, useTheme } from '@mui/material'
+import { ReactNode } from 'react'
+
+interface Props extends StackProps {
+  icon?: string
+  title?: string
+  description?: string
+  action?: ReactNode
+}
+
+export default function NoDataViewer({
+  icon = '📁',
+  title = 'No data',
+  description,
+  action,
+  ...rest
+}: Props) {
+  const { palette, spacing } = useTheme()
+
+  return (
+    <Stack
+      spacing={4}
+      alignItems={'center'}
+      width={'100%'}
+      justifyContent={'center'}
+      p={8}
+      border={1}
+      borderColor={palette.action.hover}
+      borderRadius={2}
+      sx={{ borderStyle: 'dashed', ...rest.sx }}
+      {...rest}
+    >
+      <Stack
+        alignItems={'center'}
+        justifyContent={'center'}
+        bgcolor={palette.action.active}
+        borderRadius={250}
+        width={spacing(12)}
+        height={spacing(12)}
+      >
+        <Typography variant='subtitle1'>{icon}</Typography>
+      </Stack>
+      <Stack spacing={1} textAlign={'center'}>
+        <Typography variant='body3'>{title}</Typography>
+        {description && (
+          <Typography variant='body3' color={palette.text.secondary}>
+            {description}
+          </Typography>
+        )}
+      </Stack>
+      {action}
+    </Stack>
+  )
+}
