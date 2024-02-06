851e96f34ad0eba4700a76b899fb22e4f8036199
ardier16 <ardier16@gmail.com>
2024-02-06 19:28:40 +0200

    Update layout

---
+++ src/pages/Rewards/components/RewardsLayout.tsx
@@ -0,0 +1,39 @@
+import { Stack, Typography } from '@mui/material'
+import { PropsWithChildren } from 'react'
+
+import { PageTitles } from '@/common'
+import { RoutePaths } from '@/enums'
+import { UiButton, UiIcon, UiNavTabs } from '@/ui'
+
+export default function RewardsLayout({ children }: PropsWithChildren) {
+  return (
+    <Stack spacing={6}>
+      <PageTitles title='Rewards' />
+      <Stack direction={'row'} justifyContent={'space-between'}>
+        <UiNavTabs
+          width={'max-content'}
+          tabs={[
+            { label: 'Active Tasks', route: RoutePaths.RewardsActive },
+            { label: 'History', route: RoutePaths.RewardsHistory },
+          ]}
+        />
+        <UiButton
+          component={'a'}
+          href='https://rarime.com'
+          target='_blank'
+          rel='noreferrer noopener'
+          variant='text'
+          size='small'
+          color={'secondary'}
+        >
+          <Stack direction={'row'} spacing={2} alignItems={'center'}>
+            <UiIcon componentName='openInNew' size={4} />
+            <Typography variant={'buttonSmall'}>Learn More</Typography>
+          </Stack>
+        </UiButton>
+      </Stack>
+
+      {children}
+    </Stack>
+  )
+}
--- src/pages/Rewards/index.tsx
+++ src/pages/Rewards/index.tsx
@@ -1,55 +1,40 @@
-import { Box, Stack, Typography } from '@mui/material'
+import { Box } from '@mui/material'
 import { Navigate } from 'react-router-dom'
 
-import { PageTitles } from '@/common'
 import { RoutePaths } from '@/enums'
 import { useNestedRoutes } from '@/hooks'
-import { UiButton, UiIcon, UiNavTabs } from '@/ui'
+
+import RewardsLayout from './components/RewardsLayout'
 
 export default function Rewards() {
-  const routes = useNestedRoutes(RoutePaths.Rewards, [
+  return useNestedRoutes(RoutePaths.Rewards, [
     {
       index: true,
       element: <Navigate replace to={RoutePaths.RewardsActive} />,
     },
     {
       path: RoutePaths.RewardsActive,
-      element: <Box>active</Box>,
+      element: (
+        <RewardsLayout>
+          <Box>active</Box>
+        </RewardsLayout>
+      ),
     },
     {
       path: RoutePaths.RewardsHistory,
-      element: <Box>history</Box>,
+      element: (
+        <RewardsLayout>
+          <Box>history</Box>
+        </RewardsLayout>
+      ),
+    },
+    {
+      path: RoutePaths.RewardsActiveId,
+      element: <Box>active id</Box>,
+    },
+    {
+      path: '*',
+      element: <Navigate replace to={RoutePaths.RewardsActive} />,
     },
   ])
-
-  return (
-    <Stack spacing={6}>
-      <PageTitles title='Rewards' />
-      <Stack direction={'row'} justifyContent={'space-between'}>
-        <UiNavTabs
-          width={'max-content'}
-          tabs={[
-            { label: 'Active Tasks', route: RoutePaths.RewardsActive },
-            { label: 'History', route: RoutePaths.RewardsHistory },
-          ]}
-        />
-        <UiButton
-          component={'a'}
-          href='https://rarime.com'
-          target='_blank'
-          rel='noreferrer noopener'
-          variant='text'
-          size='small'
-          color={'secondary'}
-        >
-          <Stack direction={'row'} spacing={2} alignItems={'center'}>
-            <UiIcon componentName='openInNew' size={4} />
-            <Typography variant={'buttonSmall'}>Learn More</Typography>
-          </Stack>
-        </UiButton>
-      </Stack>
-
-      {routes}
-    </Stack>
-  )
 }
