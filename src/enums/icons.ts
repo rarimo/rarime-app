d8ee1e2536047ce9b5d84b4ee87ff448a66441b8
Maksym <ardier16@gmail.com>
2024-02-13 11:36:29 +0200

    Export identity (#46)
    * Export identity
    
    * Update icon

--- src/enums/icons.ts
+++ src/enums/icons.ts
@@ -15,6 +15,7 @@ import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline'
 import FolderOff from '@mui/icons-material/FolderOff'
 import InfoIcon from '@mui/icons-material/Info'
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import Key from '@mui/icons-material/Key'
 import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined'
 import Layers from '@mui/icons-material/Layers'
 import LightModeOutlined from '@mui/icons-material/LightModeOutlined'
@@ -58,6 +59,7 @@ export const ICON_COMPONENTS = {
   folderOff: FolderOff,
   info: InfoIcon,
   infoOutlined: InfoOutlinedIcon,
+  key: Key,
   keyboardArrowDownOutlined: KeyboardArrowDownOutlinedIcon,
   layers: Layers,
   lightModeOutlined: LightModeOutlined,
