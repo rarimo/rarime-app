10e45632f24703f874b0263fe97e33a7cdf90929
Maksym <ardier16@gmail.com>
2024-03-12 12:10:26 +0200

    New auth layout (#50)
    * Update auth layout
    
    * Update sign in page
    
    * Update styles

---
+++ src/layouts/AuthLayout.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, Stack, useTheme } from '@mui/material'
+import { PropsWithChildren } from 'react'
+
+import { config } from '@/config'
+import { vh } from '@/theme/helpers'
+
+const PublicLayout = ({ children }: PropsWithChildren) => {
+  const { palette, spacing } = useTheme()
+
+  return (
+    <Stack direction={'row'} height={vh(100)}>
+      <Stack pl={14} pr={8} py={8} flex={1}>
+        <Box flex={1} position={'relative'}>
+          <Stack direction={'row'} justifyContent={'space-between'} alignItems={'center'}>
+            <Box component={'img'} src='/branding/logo-sign-in.svg' alt={config.APP_NAME} />
+            <Button
+              component={'a'}
+              href={'https://rarime.com'}
+              target={'_blank'}
+              variant='text'
+              color={'secondary'}
+              size={'medium'}
+            >
+              Visit Landing page
+            </Button>
+          </Stack>
+
+          <Stack
+            position={'absolute'}
+            top={'50%'}
+            left={'50%'}
+            width={'100%'}
+            maxWidth={spacing(100)}
+            sx={{ transform: 'translate(-50%, -50%)' }}
+          >
+            {children}
+          </Stack>
+        </Box>
+      </Stack>
+
+      <Stack
+        justifyContent={'center'}
+        alignItems={'end'}
+        pl={8}
+        bgcolor={palette.additional.pureBlack}
+      >
+        <Box
+          component={'img'}
+          src='/imgs/dashboard.png'
+          alt={'Dashboard image'}
+          height={vh(90)}
+          sx={{ objectFit: 'contain' }}
+        />
+      </Stack>
+    </Stack>
+  )
+}
+
+export default PublicLayout
