1ef633f323c2a334aec4bec34e37e20a9d2b3457
ardier16 <ardier16@gmail.com>
2024-03-14 16:17:17 +0200

    Add script to package.json

--- scripts/generate-icons.mjs
+++ scripts/generate-icons.mjs
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 import fs from 'fs'
-import camelCase from 'lodash/camelCase.js';
-import upperFirst from 'lodash/upperFirst.js';
+import camelCase from 'lodash/camelCase.js'
+import upperFirst from 'lodash/upperFirst.js'
 import path, { dirname } from 'path'
-import { fileURLToPath } from "url";
+import { fileURLToPath } from 'url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -11,8 +11,7 @@ const __dirname = dirname(__filename)
 const iconsDir = path.resolve(__dirname, '../src/assets/icons')
 const iconsEnumFile = path.resolve(__dirname, '../src/enums/icons.ts')
 
-const ICONS_ENUM_FILE_TEMPLATE =
-`import { SvgIcon } from '@mui/material'
+const ICONS_ENUM_FILE_TEMPLATE = `import { SvgIcon } from '@mui/material'
 
 export enum Icons {
   %s
@@ -21,8 +20,9 @@ export enum Icons {
 export const ICON_COMPONENTS: Record<string, typeof SvgIcon> = {}
 `
 
-function generateIconsEnum () {
-  const iconNames = fs.readdirSync(iconsDir)
+function generateIconsEnum() {
+  const iconNames = fs
+    .readdirSync(iconsDir)
     .filter(file => file.endsWith('.svg'))
     .map(file => file.replace('-icon.svg', ''))
 
@@ -35,8 +35,9 @@ function generateIconsEnum () {
   console.log(`Icons enum saved to ${iconsEnumFile}`)
 }
 
-function renameIconFiles () {
-  const iconsToRename = fs.readdirSync(iconsDir)
+function renameIconFiles() {
+  const iconsToRename = fs
+    .readdirSync(iconsDir)
     .filter(file => file.endsWith('.svg'))
     .filter(file => !file.endsWith('-icon.svg'))
 
