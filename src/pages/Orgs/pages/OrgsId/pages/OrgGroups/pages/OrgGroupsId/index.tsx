8f3209162be62dc8ecf99045c84db0ba1fc79c5f
Maksym <ardier16@gmail.com>
2024-03-14 13:49:24 +0200

    Import identity modal (#54)
    * Update import modal
    
    * Rename drawer components to dialog

--- src/pages/Orgs/pages/OrgsId/pages/OrgGroups/pages/OrgGroupsId/index.tsx
+++ src/pages/Orgs/pages/OrgsId/pages/OrgGroups/pages/OrgGroupsId/index.tsx
@@ -1,4 +1,4 @@
-import { Stack } from '@mui/material'
+import { Drawer, Stack } from '@mui/material'
 import trimEnd from 'lodash/trimEnd'
 import { useCallback, useMemo, useState } from 'react'
 import { generatePath, Navigate } from 'react-router-dom'
@@ -14,7 +14,7 @@ import { RoutePaths } from '@/enums'
 import { useLoading, useNestedRoutes } from '@/hooks'
 import { useOrgDetails } from '@/pages/Orgs/pages/OrgsId/hooks'
 import { useOrgGroupDetails } from '@/pages/Orgs/pages/OrgsId/pages/OrgGroups/hooks'
-import { UiButton, UiDrawer, UiIcon } from '@/ui'
+import { UiButton, UiIcon } from '@/ui'
 
 import { InviteMemberForm, List } from './components'
 
@@ -166,13 +166,13 @@ export default function OrgGroupsId() {
       />
       <Stack flex={1}>{routes}</Stack>
 
-      <UiDrawer open={isInviteDrawerShown} onClose={() => setIsInviteDrawerShown(false)}>
+      <Drawer open={isInviteDrawerShown} onClose={() => setIsInviteDrawerShown(false)}>
         <InviteMemberForm
           onMemberInvitationCreated={handleMemberInvitationCreated}
           maxWidth={theme => theme.spacing(100)}
           p={6}
         />
-      </UiDrawer>
+      </Drawer>
     </Stack>
   )
 }
