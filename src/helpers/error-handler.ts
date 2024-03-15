81744154e31776a6bd9edaa2a1cbf20a5053da95
ardier16 <ardier16@gmail.com>
2024-03-15 16:19:05 +0200

    Fix create identity

--- src/helpers/error-handler.ts
+++ src/helpers/error-handler.ts
@@ -6,12 +6,12 @@ import { bus } from './event-bus'
 
 export class ErrorHandler {
   static isError(error: unknown): error is Error {
-    return error instanceof Error
+    return error instanceof Error || (error instanceof Object && 'message' in error)
   }
 
   static process(error: unknown, message = ''): void {
     if (!ErrorHandler.isError(error)) return
-    bus.emit(BusEvents.error, { message })
+    bus.emit(BusEvents.error, { message: message || error.message })
     ErrorHandler.processWithoutFeedback(error)
   }
 
