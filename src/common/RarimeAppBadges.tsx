6aac94f321602839affc3e9b5859546cf9c88b50
ardier16 <ardier16@gmail.com>
2024-03-19 17:19:13 +0200

    Extract app badge component

--- src/common/RarimeAppBadges.tsx
+++ src/common/RarimeAppBadges.tsx
@@ -1,36 +1,54 @@
 import { Box, Stack, StackProps, useTheme } from '@mui/material'
+import { useMemo } from 'react'
 
 import { config } from '@/config'
 
-export default function RarimeAppBadges(props: StackProps) {
+type AppBadgeType = 'app-store' | 'google-play'
+interface AppBadgeProps {
+  type: AppBadgeType
+  link: string
+}
+
+function AppBadge({ type, link }: AppBadgeProps) {
   const { spacing } = useTheme()
 
+  const imgSrc = useMemo(() => {
+    const srcRecord: Record<AppBadgeType, string> = {
+      'app-store': '/imgs/app-store-badge.svg',
+      'google-play': '/imgs/google-play-badge.svg',
+    }
+
+    return srcRecord[type]
+  }, [type])
+
+  const imgAlt = useMemo(() => {
+    const altRecord: Record<AppBadgeType, string> = {
+      'app-store': 'App Store',
+      'google-play': 'Google Play',
+    }
+
+    return altRecord[type]
+  }, [type])
+
+  return link ? (
+    <Stack component='a' href={link} target='_blank' rel='noreferrer'>
+      <Box
+        component='img'
+        src={imgSrc}
+        alt={imgAlt}
+        height={spacing(10)}
+        width='auto'
+        sx={{ objectFit: 'contain' }}
+      />
+    </Stack>
+  ) : null
+}
+
+export default function RarimeAppBadges(props: StackProps) {
   return (
     <Stack direction='row' spacing={6} {...props}>
-      {config.APP_STORE_APP_LINK && (
-        <Stack component='a' href={config.APP_STORE_APP_LINK} target='_blank' rel='noreferrer'>
-          <Box
-            component='img'
-            src='/imgs/app-store-badge.svg'
-            alt='App Store'
-            height={spacing(10)}
-            width='auto'
-            sx={{ objectFit: 'contain' }}
-          />
-        </Stack>
-      )}
-      {config.GOOGLE_PLAY_APP_LINK && (
-        <Stack component='a' href={config.GOOGLE_PLAY_APP_LINK} target='_blank' rel='noreferrer'>
-          <Box
-            component='img'
-            src='/imgs/google-play-badge.svg'
-            alt='Google Play'
-            height={spacing(10)}
-            width='auto'
-            sx={{ objectFit: 'contain' }}
-          />
-        </Stack>
-      )}
+      <AppBadge type='app-store' link={config.APP_STORE_APP_LINK} />
+      <AppBadge type='google-play' link={config.GOOGLE_PLAY_APP_LINK} />
     </Stack>
   )
 }
