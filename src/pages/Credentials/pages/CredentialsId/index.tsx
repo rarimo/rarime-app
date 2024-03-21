29e930b10d0589112019ef6799cde1316f664aa1
ardier16 <ardier16@gmail.com>
2024-03-21 15:36:15 +0200

    Fix credential nav buttons

--- src/pages/Credentials/pages/CredentialsId/index.tsx
+++ src/pages/Credentials/pages/CredentialsId/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Paper, Stack, useTheme } from '@mui/material'
+import { Box, Button, IconButton, Paper, Stack, useTheme } from '@mui/material'
 import { useCallback, useMemo, useState } from 'react'
 import { generatePath, NavLink, useParams } from 'react-router-dom'
 
@@ -15,7 +15,7 @@ import { ActionButton } from './components'
 import VcInfoModal from './components/VcInfoModal'
 
 export default function CredentialsId() {
-  const { palette, spacing } = useTheme()
+  const { spacing } = useTheme()
   const { claimId = '' } = useParams<{ claimId: string }>()
   const { vcs, issuersDetails } = useCredentialsState()
 
@@ -90,21 +90,27 @@ export default function CredentialsId() {
         {vc && issuerDetails ? (
           <Stack spacing={6}>
             <Stack spacing={6} direction='row' alignItems='center' alignSelf='center'>
-              <Box
+              <IconButton
                 component={NavLink}
                 to={prevVCPath}
-                visibility={isFirstVC ? 'hidden' : 'visible'}
+                color='secondary'
+                sx={{ visibility: isFirstVC ? 'hidden' : 'visible', transitionProperty: 'color' }}
               >
-                <UiIcon name={Icons.CaretLeft} size={5} sx={{ color: palette.text.secondary }} />
-              </Box>
+                <UiIcon name={Icons.CaretLeft} size={5} />
+              </IconButton>
 
               <Box width={spacing(100)}>
                 <CredentialCard vc={vc} issuerDetails={issuerDetails} />
               </Box>
 
-              <Box component={NavLink} to={nextVCPath} visibility={isLastVC ? 'hidden' : 'visible'}>
-                <UiIcon name={Icons.CaretRight} size={5} sx={{ color: palette.text.secondary }} />
-              </Box>
+              <IconButton
+                component={NavLink}
+                to={nextVCPath}
+                color='secondary'
+                sx={{ visibility: isLastVC ? 'hidden' : 'visible', transitionProperty: 'color' }}
+              >
+                <UiIcon name={Icons.CaretRight} size={5} />
+              </IconButton>
             </Stack>
 
             <Stack spacing={10} direction='row' justifyContent='center'>
