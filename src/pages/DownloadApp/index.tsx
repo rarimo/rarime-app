3b602e423252f6f12fcb0db01b82c0647dc32849
ardier16 <ardier16@gmail.com>
2024-06-18 14:12:38 +0300

    Update download app page

--- src/pages/DownloadApp/index.tsx
+++ src/pages/DownloadApp/index.tsx
@@ -1,36 +1,72 @@
-import { Paper, Stack, Typography, useTheme } from '@mui/material'
-import { useEffect } from 'react'
-import { Navigate } from 'react-router-dom'
+import { IconButton, Paper, Stack, Typography, useTheme } from '@mui/material'
+import { QRCode } from 'react-qrcode-logo'
+import { useSearchParams } from 'react-router-dom'
 
 import { RarimeAppBadges } from '@/common'
-import { config } from '@/config'
-import { RoutePaths } from '@/enums'
-import { isAndroid, isIos, isMobile } from '@/helpers'
+import { Icons } from '@/enums'
+import { isMobile } from '@/helpers'
+import { useCopyToClipboard } from '@/hooks'
+import { UiIcon } from '@/ui'
 
 export default function DownloadApp() {
   const { palette } = useTheme()
+  const [searchParams] = useSearchParams()
+  const { isCopied, copy } = useCopyToClipboard()
 
-  useEffect(() => {
-    if (isIos() && config.APP_STORE_APP_LINK) {
-      location.replace(config.APP_STORE_APP_LINK)
-    }
-    if (isAndroid() && config.GOOGLE_PLAY_APP_LINK) {
-      location.replace(config.GOOGLE_PLAY_APP_LINK)
-    }
-  }, [])
+  const code = searchParams.get('code')
 
-  return isMobile() ? (
-    <Paper component={Stack} spacing={6} sx={{ borderRadius: 2 }}>
-      <Stack spacing={1}>
-        <Typography variant='subtitle2'>Download Rarime App</Typography>
-        <Typography variant='body3' color={palette.text.secondary}>
-          Manage your identity credentials, generate Zero-Knowledge Proofs and share without
-          exposing personal data
-        </Typography>
-      </Stack>
+  return (
+    <Paper
+      component={Stack}
+      justifyContent='center'
+      alignItems='center'
+      gap={4}
+      sx={{ borderRadius: 2 }}
+    >
+      {!isMobile() && (
+        <QRCode
+          logoImage='/branding/logo.svg'
+          logoPadding={5}
+          logoOpacity={1}
+          removeQrCodeBehindLogo
+          value={location.href}
+          size={150}
+        />
+      )}
+      <Typography variant='subtitle2'>Download RariMe App</Typography>
       <RarimeAppBadges spacing={4} />
+      <Stack gap={2}>
+        <Typography variant='body3'>1. Download RariMe app on your phone</Typography>
+        <Typography variant='body3'>2. Open the app and create your account</Typography>
+        {code && (
+          <Typography variant='body3'>3. Join rewards program by entering the code</Typography>
+        )}
+      </Stack>
+      {code && (
+        <Stack
+          width='100%'
+          direction='row'
+          gap={2}
+          justifyContent='center'
+          p={3}
+          bgcolor={palette.action.active}
+          border={2}
+          borderColor={palette.divider}
+          borderRadius={2}
+        >
+          <Typography
+            variant='subtitle2'
+            color={palette.text.primary}
+            letterSpacing={3}
+            textAlign='center'
+          >
+            {code}
+          </Typography>
+          <IconButton color={isCopied ? 'success' : 'primary'} onClick={() => copy(code)}>
+            <UiIcon name={isCopied ? Icons.Check : Icons.CopySimple} size={5} />
+          </IconButton>
+        </Stack>
+      )}
     </Paper>
-  ) : (
-    <Navigate to={RoutePaths.Dashboard} replace />
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,68 +1,17 @@
-import { lazy, Suspense, useCallback, useMemo } from 'react'
-import {
-  createBrowserRouter,
-  LoaderFunctionArgs,
-  Navigate,
-  Outlet,
-  redirect,
-  RouterProvider,
-} from 'react-router-dom'
+import { lazy, Suspense, useMemo } from 'react'
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom'
 
 import { RoutePaths } from '@/enums'
 import { useAuth } from '@/hooks'
 
-import { createDeepPath, isMobile } from './helpers'
 import AuthLayout from './layouts/AuthLayout'
 import MainLayout from './layouts/MainLayout'
 
 export const AppRoutes = () => {
-  const SignIn = lazy(() => import('@/pages/SignIn'))
-  const Credentials = lazy(() => import('@/pages/Credentials'))
-  const Dashboard = lazy(() => import('@/pages/Dashboard'))
-  const UiKit = lazy(() => import('@/pages/UiKit'))
-  const VerifyProofAlias = lazy(() => import('@/pages/VerifyProofAlias'))
-  const AcceptInvitation = lazy(() => import('@/pages/AcceptInvitation'))
-  const Rewards = lazy(() => import('@/pages/Rewards'))
-  const Wallet = lazy(() => import('@/pages/Wallet'))
   const RewardsInvitationAlias = lazy(() => import('@/pages/RewardsInvitationAlias'))
   const DownloadApp = lazy(() => import('@/pages/DownloadApp'))
 
-  const { isAuthorized, logout } = useAuth()
-
-  const mobileGuard = useCallback(() => {
-    return isMobile() ? redirect(RoutePaths.DownloadApp) : null
-  }, [])
-
-  const signInGuard = useCallback(
-    ({ request }: LoaderFunctionArgs) => {
-      const requestUrl = new URL(request.url)
-
-      const from = requestUrl.searchParams.get('from')
-
-      return isAuthorized
-        ? redirect(from ? `${from}${requestUrl.search}` : RoutePaths.Root)
-        : mobileGuard()
-    },
-    [isAuthorized, mobileGuard],
-  )
-  const authProtectedGuard = useCallback(
-    ({ request }: LoaderFunctionArgs) => {
-      // If the user is not logged in and tries to access protected route, we redirect
-      // them to sign in with a `from` parameter that allows login to redirect back
-      // to this page upon successful authentication
-      if (!isAuthorized) {
-        logout()
-
-        const requestUrl = new URL(request.url)
-        requestUrl.searchParams.set('from', requestUrl.pathname)
-
-        return redirect(`${RoutePaths.SignIn}${requestUrl.search}`)
-      }
-
-      return mobileGuard()
-    },
-    [isAuthorized, logout, mobileGuard],
-  )
+  const { isAuthorized } = useAuth()
 
   const LayoutComponent = useMemo(() => {
     return isAuthorized ? MainLayout : AuthLayout
@@ -80,43 +29,6 @@ export const AppRoutes = () => {
       ),
       children: [
         {
-          path: createDeepPath(RoutePaths.Dashboard),
-          loader: authProtectedGuard,
-          element: <Dashboard />,
-        },
-        {
-          path: createDeepPath(RoutePaths.Orgs),
-          element: <Navigate replace to={RoutePaths.Root} />,
-        },
-        {
-          path: createDeepPath(RoutePaths.Credentials),
-          loader: authProtectedGuard,
-          element: <Credentials />,
-        },
-        {
-          path: createDeepPath(RoutePaths.Rewards),
-          loader: authProtectedGuard,
-          element: <Rewards />,
-        },
-        {
-          path: createDeepPath(RoutePaths.Wallet),
-          loader: authProtectedGuard,
-          element: <Wallet />,
-        },
-        {
-          path: createDeepPath(RoutePaths.UiKit),
-          element: <UiKit />,
-        },
-        {
-          path: createDeepPath(RoutePaths.SignIn),
-          loader: signInGuard,
-          element: <SignIn />,
-        },
-        {
-          path: RoutePaths.VerifyProofAlias,
-          element: <VerifyProofAlias />,
-        },
-        {
           path: RoutePaths.RewardsInvitationAlias,
           element: <RewardsInvitationAlias />,
         },
@@ -125,13 +37,8 @@ export const AppRoutes = () => {
           element: <DownloadApp />,
         },
         {
-          path: createDeepPath(RoutePaths.AcceptInvitation),
-          element: <AcceptInvitation />,
-          loader: authProtectedGuard,
-        },
-        {
           path: RoutePaths.Root,
-          element: <Navigate replace to={RoutePaths.Dashboard} />,
+          element: <Navigate replace to={RoutePaths.DownloadApp} />,
         },
         {
           path: '*',
