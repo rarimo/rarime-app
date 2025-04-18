3cd53972e1379509624124d04131b612bf3ae10d
ardier16 <ardier16@gmail.com>
2025-04-18 14:40:21 +0300

    Fix external QR code view

--- src/pages/ExternalRequest/index.tsx
+++ src/pages/ExternalRequest/index.tsx
@@ -1,4 +1,4 @@
-import { Paper, Stack, Typography } from '@mui/material'
+import { ButtonBase, Paper, Stack, Typography } from '@mui/material'
 import { QRCode } from 'react-qrcode-logo'
 import { useLocation } from 'react-router-dom'
 
@@ -7,11 +7,20 @@ import { isMobile } from '@/utils/device'
 
 export default function ExternalRequest() {
   const location = useLocation()
+  const rarimeLink = `rarime://external${location.search}`
+
+  const openRarimeLink = () => {
+    if (isMobile()) {
+      window.location.href = rarimeLink
+    }
+  }
 
   return (
     <Paper component={Stack} justifyContent='center' alignItems='center' gap={4}>
       <Typography variant='h6'>Scan with RariMe App</Typography>
-      {!isMobile() && <QRCode value={`rarime://external${location.search}`} size={200} />}
+      <ButtonBase onClick={openRarimeLink}>
+        <QRCode value={rarimeLink} size={200} />
+      </ButtonBase>
       <RarimeAppBadges spacing={4} />
     </Paper>
   )
